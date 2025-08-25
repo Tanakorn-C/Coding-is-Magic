477b266a561b15018cd5444386db92f977393c8d
Tanakorn-C <tanakorncn.159@gmail.com>
2025-08-25 18:55:57 +0700

    Init

---
+++ Player/Script/player.gd
@@ -0,0 +1,21 @@
+class_name PLayer extends CharacterBody2D
+
+
+var move_speed : float = 100.0
+
+
+
+func _ready():
+	pass
+	
+func _process(delta):
+	
+		var direction : Vector2 = Vector2.ZERO
+		direction.x = Input.get_action_strength("right"	) - Input.get_action_strength("left")
+		direction.x = Input.get_action_strength("up"	) - Input.get_action_strength("down")
+		
+		velocity = direction * move_speed
+		
+		pass
+		
+	
